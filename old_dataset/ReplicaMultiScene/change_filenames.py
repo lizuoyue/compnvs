06e2e99b5ef1447ccaada76e131ced85cc158a45
shinkyo0513 <zqlee0513@gmail.com>
2022-11-11 21:58:07 +0900

    delete useless files

--- old_dataset/ReplicaMultiScene/change_filenames.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for i in range(0, 45):
-    os.system(f'mv scene{i:02d}/init_voxel/fine_points_0.1.txt scene{i:02d}/init_voxel/fine_points_0.1_aligned.txt')
-    continue
-    for e, j in enumerate(range(i * 250, (i + 1) * 250)):
-        os.system(f'mv scene{i:02d}/rgb/0_{j:04d}.png scene{i:02d}/rgb/0_{e:04d}.png')
-        os.system(f'mv scene{i:02d}/depth/0_{j:04d}.png scene{i:02d}/depth/0_{e:04d}.png')
-        os.system(f'mv scene{i:02d}/pose/0_{j:04d}.txt scene{i:02d}/pose/0_{e:04d}.txt')
